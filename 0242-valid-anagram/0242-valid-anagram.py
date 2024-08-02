41afa1ae000019fd97ac31d2c20bc894d26e16a2
algoace99 <algoace99@gmail.com>
2024-08-02 21:21:14 +0530

    Time: 42 ms (86.85%), Space: 17 MB (43.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,10 +7,23 @@ class Solution:
         if s==t:
             return True
         return False
-        '''
+
         for key,value in s.items():
             if(key not in t):
                 return False
             elif(t[key]!=value):
                 return False
         return True
+        '''
+        if len(s) != len(t): return False
+
+        sCount = collections.Counter(s)
+        tCount = collections.Counter(t)
+
+        for c in sCount:
+            if c not in tCount:
+                return False
+            if tCount[c] != sCount[c]:
+                return False
+
+        return True 
