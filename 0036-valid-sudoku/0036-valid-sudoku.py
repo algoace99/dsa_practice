d9af134bc250f5f4833e29df07d0a2c039594c0e
algoace99 <algoace99@gmail.com>
2024-08-03 14:39:51 +0530

    Time: 98 ms (24.36%), Space: 16.6 MB (41.54%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,34 @@
+class Solution:
+    def validateArrayContainsDuplicates(self, l: List[str]) -> bool:
+        # return True if contains duplicates
+        c = Counter(l)
+        for key, value in c.items():
+            if key != "." and value > 1:
+                return True
+        return False
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check rows
+        for row in board:
+            if self.validateArrayContainsDuplicates(row):
+                return False
+
+        # Check columns
+        for i in range(len(board)):
+            column = []
+            for j in range(len(board[0])):
+                column.append(board[j][i])
+            if self.validateArrayContainsDuplicates(column):
+                return False
+
+        # Check 3x3 subgrids
+        for i in range(0, len(board), 3):
+            for j in range(0, len(board[0]), 3):
+                subgrid = []
+                for k in range(3):
+                    for l in range(3):
+                        subgrid.append(board[i+k][j+l])
+                if self.validateArrayContainsDuplicates(subgrid):
+                    return False
+
+        return True
