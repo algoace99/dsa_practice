ac263268d6e376825045d0c2d5654f010b90cc2a
algoace99 <algoace99@gmail.com>
2024-08-15 15:33:27 +0530

    Time: 41 ms (22.97%), Space: 16.5 MB (15.95%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,3 +1,17 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        return (True if math.floor(math.sqrt(num))**2==num else False)
+        #return (True if math.floor(math.sqrt(num))**2==num else False)
+
+        #two pointer approach without inbuilt methods
+        if num<2: return True
+        l,r=0,num//2
+        while l<=r:
+            m=l+(r-l)//2
+            sq=m*m
+            if sq==num:
+                return True
+            elif sq<num:
+                l=m+1
+            else:
+                r=m-1
+        return False
