b0d29620c3876f41465999c3760d07de1f753132
algoace99 <algoace99@gmail.com>
2024-09-03 08:02:22 +0530

    Time: 37 ms (66.35%), Space: 16.6 MB (37%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -3,9 +3,8 @@ class Solution:
         digit_sum=""
         for i in s:
             digit_sum+=str((ord(i)-96))
-        final_sum=0
+
         for _ in range(k):
-            final_sum=sum(list(map(int,list(str(digit_sum)))))
-            digit_sum=final_sum
+            digit_sum=sum(list(map(int,list(str(digit_sum)))))
             
         return int(digit_sum)
