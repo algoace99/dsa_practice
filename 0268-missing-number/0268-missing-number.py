cacd5df677e92a337c05a253634017536cc50d9f
algoace99 <algoace99@gmail.com>
2024-09-21 20:45:26 +0530

    Time: 113 ms (49.49%), Space: 17.8 MB (18.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -8,6 +8,13 @@ class Solution:
             if i not in s:
                 return i
         '''
+        ans = 0
+        for i in range(1,len(nums)+1):
+            ans = ans ^ i
+        for n in nums:
+            ans = ans ^ n
+        return ans
+
         # Time - O(n)
         # Space - O(1)
-        return (len(nums)*(len(nums)+1)//2)-sum(nums)
+        #return (len(nums)*(len(nums)+1)//2)-sum(nums)
