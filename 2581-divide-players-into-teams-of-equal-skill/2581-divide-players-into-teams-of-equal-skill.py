affc124ae11499df49f5818c4c1f38e2c8d10e8c
algoace99 <algoace99@gmail.com>
2024-10-04 19:48:01 +0530

    Time: 394 ms (75.04%), Space: 29.4 MB (94.16%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -1,14 +1,11 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
+        '''
         tot = sum(skill)
-
         if tot % (len(skill)//2) != 0:
             return -1
-        
         count = Counter(skill)
-
         req = tot//(len(skill)/2)
-
         sum_ans = 0
         for s in skill:
             if count[s] == 0:
@@ -20,3 +17,15 @@ class Solution:
             count[s] -= 1
             count[diff] -= 1
         return int(sum_ans)
+        '''
+        # Using Sorting, without extra space
+        skill.sort()
+        n = len(skill)
+        ans = 0
+        req = skill[0] + skill[-1]
+
+        for i in range(n//2):
+            if skill[i] + skill[-i-1] != req:
+                return -1
+            ans += skill[i]*skill[-i-1]
+        return ans
