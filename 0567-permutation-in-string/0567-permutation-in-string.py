c4d09840434c6ab2bfa1f586b746576dbd3092f5
algoace99 <algoace99@gmail.com>
2024-10-05 07:43:30 +0530

    Time: 93 ms (35.25%), Space: 16.6 MB (88.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,18 +3,15 @@ class Solution:
         s1_count = Counter(s1)
         s2_count = Counter(s2[:len(s1)])
         
-        if s1_count == s2_count:
-            return True
-        
-        for i in range(len(s1), len(s2)):
-            left_char = s2[i - len(s1)]
-            s2_count[left_char] -= 1
-            if s2_count[left_char] == 0:
-                del s2_count[left_char]
-
-            new_char = s2[i]
-            s2_count[new_char] += 1
-            
+        for i in range(len(s2) - len(s1) + 1):
             if s1_count == s2_count:
                 return True
+            if i + len(s1) < len(s2):
+                left_char = s2[i]
+                s2_count[left_char] -= 1
+                if s2_count[left_char] == 0:
+                    del s2_count[left_char]
+                new_char = s2[i + len(s1)]
+                s2_count[new_char] += 1
+        
         return False
