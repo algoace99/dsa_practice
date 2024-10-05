b7262f4b1cb5ce79fcbb035d7660264f8ce1cc71
algoace99 <algoace99@gmail.com>
2024-10-05 06:49:02 +0530

    Time: 106 ms (81.3%), Space: 18 MB (15.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,21 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        '''
-        # Time - O(n)
-        # Space - O(n)
-        s = set(nums)
-        for i in range(len(nums)+1):
-            if i not in s:
-                return i
-        '''
-        '''
-        ans = 0
-        for i in range(1,len(nums)+1):
-            ans = ans ^ i
-        for n in nums:
-            ans = ans ^ n
-        return ans
-        '''
-        # Time - O(n)
-        # Space - O(1)
-        return (len(nums)*(len(nums)+1)//2)-sum(nums)
+        n = len(nums)
+        return int((n*(n+1))/2 - sum(nums))
