5c7388932353b1bddc10e728f98605e1f5a5e1fe
algoace99 <algoace99@gmail.com>
2024-10-05 07:03:13 +0530

    Time: 1178 ms (17.4%), Space: 16.7 MB (22.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1_count = Counter(s1)
+        s2_count = {}
         for i in range(0,len(s2)-len(s1)+1):
             s2_count = Counter(s2[i:i+len(s1)])
             if s1_count == s2_count:
