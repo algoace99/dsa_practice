85f6c3297c2da3b1d02f6fb695ff49ab42d19f3b
algoace99 <algoace99@gmail.com>
2024-10-06 21:15:46 +0530

    Time: 102 ms (62.1%), Space: 17.4 MB (61.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        '''
         index = []
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
@@ -13,3 +14,20 @@ class Solution:
             for i in range(len(matrix)):
                 matrix[i][ele[1]] = 0
         return matrix
+        '''
+        row = [1]*len(matrix)
+        col = [1]*len(matrix[0])
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j]==0:
+                    row[i] = 0
+                    col[j] = 0
+        for i in range(len(row)):
+            if row[i] == 0:
+                matrix[i] = [0]*len(col)
+        for i in range(len(col)):
+            if col[i] == 0:
+                for c in range(len(row)):
+                    matrix[c][i] = 0
+        
