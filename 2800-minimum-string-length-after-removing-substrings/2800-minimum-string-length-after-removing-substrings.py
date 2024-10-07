b14d6801db34fee180cf59d04f5c03534171ad19
algoace99 <algoace99@gmail.com>
2024-10-07 07:05:50 +0530

    Time: 43 ms (63.91%), Space: 16.6 MB (41.77%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -1,7 +1,5 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        if len(s)==1:
-            return 1
         sta = []
         for i in s:
             if len(sta)>0 and ((i=="B" and sta[-1]=="A") or (i=="D" and sta[-1]=="C")):
