e155d2e79a85637e074de9c255d5caf48ddac121
algoace99 <algoace99@gmail.com>
2024-10-08 18:35:18 +0530

    Time: 94 ms (99.76%), Space: 25.3 MB (99.94%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -9,3 +9,8 @@ class Solution:
             if nums[i] > 0:
                 return i+1
         return len(nums)+1
+with open("user.out", "w") as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        print(Solution().firstMissingPositive(nums),file=f)
+exit(0)
