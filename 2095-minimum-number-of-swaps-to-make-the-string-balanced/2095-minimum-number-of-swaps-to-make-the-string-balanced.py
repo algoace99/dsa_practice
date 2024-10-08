a878f7846d3a5812915012235a3aacef620b751d
algoace99 <algoace99@gmail.com>
2024-10-08 06:48:56 +0530

    Time: 221 ms (57.48%), Space: 29.3 MB (19.6%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        arr = []
+        unbalanced = 0
+        for i in s:
+            if i=="[":
+                arr.append(i)
+            else:
+                if len(arr)!=0:
+                    arr.pop()
+                else:
+                    unbalanced+=1
+        return (unbalanced+1)//2
