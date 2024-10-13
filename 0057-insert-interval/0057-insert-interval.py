e410722eabb2169bdba32c10493875b2c5c42802
algoace99 <algoace99@gmail.com>
2024-10-13 09:48:36 +0530

    Time: 73 ms (69.86%), Space: 19.9 MB (27.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,5 +1,6 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        '''
         intervals.append(newInterval)
         intervals.sort(key = lambda a:a[0])
         ans = []
@@ -8,4 +9,21 @@ class Solution:
                 ans.append(i)
             else:
                 ans[-1][-1] = max(ans[-1][-1], i[1])
+        return ans
+        '''
+        ans = []
+        inserted = False 
+        for i in intervals:
+            if i[1] < newInterval[0]:
+                ans.append(i)
+            elif i[0] > newInterval[1]:
+                if not inserted:
+                    ans.append(newInterval)
+                    inserted = True
+                ans.append(i)
+            else:
+                newInterval[0] = min(newInterval[0], i[0])
+                newInterval[1] = max(newInterval[1], i[1])
+        if not inserted:
+            ans.append(newInterval)
         return ans
