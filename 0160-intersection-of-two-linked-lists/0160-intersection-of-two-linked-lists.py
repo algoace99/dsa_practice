e21034a572bcc43cff68f60e8e8ff10862f27550
algoace99 <algoace99@gmail.com>
2024-10-16 20:07:33 +0530

    Time: 77 ms (76.19%), Space: 27.2 MB (63.26%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,6 +6,7 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        '''
         s = set()
         temp = headA
         while temp:
@@ -16,4 +17,38 @@ class Solution:
             if temp in s:
                 return temp
             temp = temp.next
+        return None
+        '''
+        len_a = 0
+        temp = headA
+        while temp:
+            len_a += 1
+            temp = temp.next
+        
+        len_b = 0
+        temp = headB
+        while temp:
+            len_b += 1
+            temp = temp.next
+        
+        temp_1 = headA
+        temp_2 = headB
+
+        diff = len_a - len_b
+        if diff > 0:
+            while diff:
+                temp_1 = temp_1.next
+                diff -= 1
+        else:
+            diff = abs(diff)
+            while diff:
+                temp_2 = temp_2.next
+                diff -= 1
+        
+        while temp_1 and temp_2:
+            if temp_1 == temp_2:
+                return temp_1
+            temp_1 = temp_1.next
+            temp_2 = temp_2.next
+        
         return None
