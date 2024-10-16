c32c387ead4a97dca73377c8e46676450a63cf42
algoace99 <algoace99@gmail.com>
2024-10-16 20:30:11 +0530

    Time: 50 ms (48.1%), Space: 19.4 MB (52.19%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        '''
         temp = ListNode(0)
         temp.next = head
         prev, curr = temp, head
@@ -15,3 +16,15 @@ class Solution:
                 prev = curr
             curr = curr.next
         return temp.next
+        '''
+        while head and head.val == val:
+            head = head.next
+
+        curr = head
+        while curr and curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head
