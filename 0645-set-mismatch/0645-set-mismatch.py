aaa2758dbf7e35069640b056dd8f8936830d26d6
algoace99 <algoace99@gmail.com>
2024-10-21 19:13:57 +0530

    Time: 4 ms (99.81%), Space: 18.4 MB (35.4%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,6 +1,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        actual_sum = sum(nums)
+        unique_sum = sum(set(nums))
+        expected_sum = n * (n + 1) // 2
+
+        duplicate = actual_sum - unique_sum
+        missing = expected_sum - unique_sum
+
+        return [duplicate, missing]
+        '''
+        n = len(nums)
         total = 0
         for i in range(n):
             val = abs(nums[i])
@@ -14,3 +24,4 @@ class Solution:
         miss = dup - diff
 
         return [dup, miss]
+        '''
