c1ac0d4f2fb8694b15f78d2e2bb206c092de84a8
algoace99 <algoace99@gmail.com>
2024-10-23 12:54:11 +0530

    Time: 0 ms (100%), Space: 16.6 MB (10.73%) - LeetHub

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,16 +1,3 @@
-def sum1(a,b):
-    return a+b
-def sum2(a,b):
-    return a+b
-def sum3(a,b):
-    return a+b
-def sum4(a,b):
-    return a+b
-def sum5(a,b):
-    return a+b
-def sum6(a,b):
-    return a+b
-
 class Solution:
     def countElements(self, nums: List[int]) -> int:
         res = len(nums) - nums.count(min(nums)) - nums.count(max(nums))
