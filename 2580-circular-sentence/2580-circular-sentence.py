4c991166c7e3a998c714481185514490e7ffb1fe
algoace99 <algoace99@gmail.com>
2024-11-02 07:17:56 +0530

    Time: 0 ms (100%), Space: 16.6 MB (11.84%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.py
+++ 2580-circular-sentence/2580-circular-sentence.py
@@ -3,7 +3,6 @@ class Solution:
         if sentence[0] != sentence[-1]:
             return False
         for i in range(len(sentence)):
-            if sentence[i] == " ":
-                if sentence[i-1] != sentence[i+1]:
+            if sentence[i] == " " and  sentence[i-1] != sentence[i+1]:
                     return False
         return True
