2bba64237dbac4823f830706378512bc90527410
algoace99 <algoace99@gmail.com>
2024-11-04 12:17:09 +0530

    Time: 118 ms (96.45%), Space: 18.8 MB (95.97%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.py
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -1,13 +1,16 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        comp = []
-        count = 1
-        
-        for i in range(1, len(word) + 1):
-            if i == len(word) or word[i] != word[i - 1] or count == 9:
-                comp.append(f"{count}{word[i - 1]}")
-                count = 1
-            else:
-                count += 1
+        count, ans, prev = 0, "", word[0]
+        for ch in word:
+            if ch != prev or count == 9:
+                ans += str(count) + prev
+                prev = ch
+                count = 0
+            count += 1
+
+        if count:
+            ans += str(count) + prev
+
+        return ans
         
         return ''.join(comp)
