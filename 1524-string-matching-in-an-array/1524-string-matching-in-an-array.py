fc5a7258e7937688f9db3cd1cd8e9f8507581ab1
algoace99 <algoace99@gmail.com>
2025-01-07 21:02:32 +0530

    Time: 7 ms (13.01%), Space: 17.6 MB (32.74%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
+        # Time = O(n^2)
+        # Space = O(n)
         ans = []
         for i in range(len(words)):
             for j in range(len(words)):
