b5746de0adf52e8461525c9ee346eb2399ee77c3
algoace99 <algoace99@gmail.com>
2025-01-10 15:19:32 +0530

    Time: 433 ms (47.81%), Space: 21.1 MB (32.67%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordSubsets(self, A: 'List[str]', B: 'List[str]') -> 'List[str]':
+        def count(word):
+            ans = [0] * 26
+            for c in word:
+                ans[ord(c) - ord('a')] += 1
+            return ans
+
+        bmax = [0] * 26
+        for b in B:
+            ls = count(b)
+            for i in range(26):
+                bmax[i] = max(bmax[i], ls[i])
+        
+        ans = []
+        for a in A:
+            ls = count(a)
+            for i in range(26):
+                if ls[i] < bmax[i]:
+                    break
+            else:
+                ans.append(a)
+        return ans
