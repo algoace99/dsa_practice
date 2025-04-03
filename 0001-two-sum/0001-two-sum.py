bc6d86a9293f81c603cd1a51b485ea3192a56190
algoace99 <algoace99@gmail.com>
2025-04-03 20:17:39 +0530

    Time: 4126 ms (5%), Space: 18.5 MB (78.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,36 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''
-        # works only for sorted array
-        # Space = O(1)
-        # Time = O(n)
-        start=0
-        end=len(nums)-1
-        while(start<end):
-            if(nums[start]+nums[end]==target):
-                return [start,end]
-            elif(nums[start]+nums[end]<target):
-                start+=1
-            else:
-                end-=1
-        '''
-        '''
-        # Time = O(n*n)
-        # Space = O(1)
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        '''
-        # Space = O(n)
-        # Time = O(n)
-        h = {}
-        for i in nums:
-            h[i] = i
-        for i in range(len(nums)):
-            req = target - nums[i]
-            if req in h:
-                if req != nums[i]:
-                    return [i, nums.index(req)]
-                elif nums.count(nums[i])>1:
-                    return [n for n in range(len(nums)) if nums[n]==req][:3]
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                    return [i, j]
+        # l = 0
+        # r = len(nums)-1
+        # while l < r:
+        #     if nums[l]+nums[r] == target:
+        #         return [l, r]
+        #     elif nums[l]+nums[r] < target:
+        #         l += 1
+        #     else:
+        #         r -= 1
+        
