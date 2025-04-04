b291af8a8bd74eba9d90bfde2d6f263b67fc65f8
algoace99 <algoace99@gmail.com>
2025-04-04 07:55:50 +0530

    Time: 11 ms (29.5%), Space: 19.3 MB (52.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,39 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        '''
-        #Using sorting
-        #Time - O(n log n) and no extra space
-        nums.sort()
-        return nums[len(nums)//2]
-        '''
-
-        #Moore's voting algorithm 
-        #No extra space and O(n) time
-        count=0
-        ans=0
-        for i in nums:
-            if count==0:
-                ans=i
-                count=1
-            elif(ans==i):
-                count+=1
+        ele = nums[0]
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == ele:
+                count += 1
             else:
-                count-=1
-        return ans
-
-        '''
-        #With extra space and O(n) time
-        d={}
-        for i in nums:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-        count=0
-        k=0
-        for key,value in d.items():
-            if(count<value):
-                count=value
-                k=key
-        return k
-        '''
+                if count <= 0:
+                    ele = nums[i]
+                    count = 0
+                else:
+                    count -= 1
+        return ele
